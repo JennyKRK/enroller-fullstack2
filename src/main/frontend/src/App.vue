8b895f4400304d527f508fafb6948ee3ad37c02b
jonowak <jonowak@d17-430-pc-16.ipa.ii.agh.edu.pl>
2023-04-23 15:49:16 +0200

    afterlab

--- src/main/frontend/src/App.vue
+++ src/main/frontend/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <h1>Witaj w systemie do zapisów na zajęcia</h1>
+    <h1>Witaj w systemie do zapisów na zajęcia!</h1>
 
     <div v-if="authenticatedUsername">
       <UserPanel :username="authenticatedUsername" @logout="logMeOut()"></UserPanel>
@@ -8,7 +8,18 @@
     </div>
 
     <div v-else>
-      <LoginForm @login="(user) => logMeIn(user)"></LoginForm>
+<!--      //this part uses java script-->
+      <button @click="registering=false" :class="registering ? 'button-outline' : ''">Logowanie.</button>
+      <button @click="registering=true" :class="!registering ? 'button-outline' : ''">>Rejestracja.</button>
+
+      <LoginForm v-if="!registering" @login="(user) => logMeIn(user)"></LoginForm>
+      <LoginForm v-else @login="(user)=> register(user)" button-label="Zaloz konto"></LoginForm>
+
+<!--      <div v-if="failureLog"> Your account has not been added</div>-->
+<!--      <div v-else> OK </div>-->
+
+      <div v-if="message" class = "alert">{{message}}</div>
+
     </div>
   </div>
 </template>
@@ -18,28 +29,71 @@ import "milligram";
 import LoginForm from "./LoginForm";
 import UserPanel from "./UserPanel";
 import MeetingsPage from "./meetings/MeetingsPage";
+import RegisterForm from "@/meetings/RegisterForm";
+import axios, {isAxiosError} from "axios";
 
 export default {
   components: {LoginForm, MeetingsPage, UserPanel},
   data() {
     return {
       authenticatedUsername: '',
+      registering: false,
+      message:""
     }
   },
   methods: {
     logMeIn(user) {
-      this.authenticatedUsername = user.login;
-    },
+
+      axios.post('/api/tokens', user)
+          .then(response => {
+            // alert("Account not added. This user already exists")
+            this.authenticatedUsername = user.login;
+            const token = response.data.token;
+            axios.defaults.headers.common['Authorization'] = 'Bearer ' + token;
+            axios.get('/api/meetings').then(response => console.log(response.data));
+
+          })
+          .catch(response => {
+            // alert("unable to add")
+            this.message="nie udalo sie zalogowac"
+
+          });
+
+        }
+
+
+    ,
     logMeOut() {
       this.authenticatedUsername = '';
+      delete axios.defaults.headers.common.Authorization;
+    },
+    register(user) {
+      axios.post('/api/participants', user)
+          .then(response => {
+            // alert("Account not added. This user already exists")
+            this.message = "Twoje konto zostalo zalozone"
+
+          })
+          .catch(response => {
+            // alert("unable to add")
+            this.message = "Twoje konto nie zostalo zalozone"
+
+          });
     }
-  }
-}
+
+}}
 </script>
 
 <style>
 #app {
   max-width: 1000px;
   margin: 0 auto;
+}
+.alert{
+  border: 2px green solid;
+}
+
+.alert-red{
+  border: 2px red;
 }
 </style>
---
+++ src/main/frontend/src/meetings/RegisterForm.vue
@@ -0,0 +1,23 @@
+<template>
+
+  <form @submit.prevent="enter()">
+    <label>Your login</label>
+    <input type="text">
+    <label>Your password</label>
+    <input type="password">
+    <button type="submit">{{ buttonLabel || 'Potwierdz rejestracje' }}</button>
+  </form>
+
+</template>
+
+<script>
+export default {
+  name: "RegisterForm"
+}
+</script>
+
+<style scoped>
+
+</style>
+
+
